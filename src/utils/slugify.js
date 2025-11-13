c8c5d0191c8fbc27dab9ff3b47d5de44a4da98d8
ofemiashiru <ofemiashiru@gmail.com>
2025-11-13 00:35:27 +0000

    remove slugify util

--- src/utils/slugify.js
+++
@@ -1,8 +0,0 @@
-export const slugify = (text) => {
-  return text
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, "") // Remove non-word characters (e.g., apostrophes)
-    .replace(/[\s_-]+/g, "-") // Collapse whitespace/dashes into a single dash
-    .replace(/^-+|-+$/g, ""); // Remove leading/trailing dashes
-};
